608c3d2c507c951173c194be67e968366c9a9df5
dhagell <dhagell@users.noreply.github.com>
2018-11-13 14:48:44 -0500

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,9 @@
+//require('dotenv').config({path: __dirname + '/../.env'})
+
+module.exports = {
+	HOST: "https://ropsten.infura.io/v3/9aa5cb99edcf43f1834fdc137f7f210e",
+    PORT: 8485,
+    DEFAULTACCOUNT: 'enter',
+    PASSWORD: 'enter',
+    API_KEY: '9aa5cb99edcf43f1834fdc137f7f210e',
+}
